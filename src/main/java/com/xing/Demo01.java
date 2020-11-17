6b416db2689e01bb934f2782c00b0c1fb12c249c
xfh <2030280200@qq.com>
2020-11-17 16:25:42 +0800

    Initial commit

---
+++ src/main/java/com/xing/Demo01.java
@@ -0,0 +1,21 @@
+package com.xing;
+
+public class Demo01 {
+        public static void main(String[] args) {
+
+       //所有的标示符都是以A——Z（a-z）$符号 _下划线开头,其他的均为不符合要求的
+       //首字母符合上面的要求后，可以是任意的符号，数值
+        String Aname ="邢福豪";
+        String name ="邢福豪";
+        String $name ="邢福豪";
+        String _name ="邢福豪";
+
+
+      //  String @name ="邢福豪";
+      // String #name ="邢福豪";
+      //  String %name ="邢福豪";
+      //  String *name ="邢福豪";
+      //  String &name ="邢福豪";
+
+    }
+}
---
+++ src/main/java/com/xing/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.xing;
+
+public class HelloWorld {
+    //student是变量，可以变化的值
+    static String student="邢福豪";
+    public static void main(String[] args) {
+        //重新打基础，把面试，就业找工作的知识记住并理解了
+        System.out.println ("Hello world!");
+
+        System.out.println (student);
+    }
+
+}
