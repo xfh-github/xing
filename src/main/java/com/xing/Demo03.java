08bfdd0d17c183f0acff375e969fae39fc168984
xfh <2030280200@qq.com>
2020-11-17 18:48:51 +0800

    java八大基本数据类型

---
+++ src/main/java/com/xing/Demo03.java
@@ -0,0 +1,20 @@
+package com.xing;
+
+public class Demo03 {
+    public static void main(String[] args) {
+        //Java八大基本数据类型
+
+        //整数
+        byte  num1=1;
+        short num2=2;
+        int   num4=4; //最常用
+        long  num8=8L; //long类型要在数字后面加个大写的L
+
+        //小数：浮点数
+        float num44= 4.4f; //float类型要在数值后面加上一个f
+        double num88=8.8;
+
+    }
+
+
+}
